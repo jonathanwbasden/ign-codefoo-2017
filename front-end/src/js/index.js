51415325368624cf2c64a9cffcf1680c36af0b47
Jonathan Basden <Jonathan Basden>
2017-02-27 22:47:31 -0600

    improving look and feel.

--- front-end/src/js/index.js
+++ front-end/src/js/index.js
@@ -14,17 +14,17 @@ class App extends React.Component {
 		this.loadMoreData = this.loadMoreData.bind(this);
 		this.handleRowClick = this.handleRowClick.bind(this);
 		this.openImageUrl = this.openImageUrl.bind(this);
-		this.state = {activeClass: "videos", data: [], maxRows: 5, clickedRows: [], loaded: false, index: 0};
+		this.state = {activeClass: "videos", data: [], maxRows: 10, clickedRows: [], loaded: false, hasLoadedMoreData: false};
 	}
 
 	retrieveVideos() {
 		$.ajax({
-			url: 'http://ign-apis.herokuapp.com/videos/?startIndex=0&count=5',
+			url: 'http://ign-apis.herokuapp.com/videos/?startIndex=0&count=10',
 			type: 'GET',			
 			dataType: 'json'
 		}).done(function(data) {
 			console.dir(data);
-			this.setState({data: data.data, activeClass: "videos", maxRows: 5, clickedRows: [], loaded: true});
+			this.setState({data: data.data, activeClass: "videos", maxRows: 10, clickedRow: -1, loaded: true, hasLoadedMoreData: false});
 		}.bind(this))
 		.fail(function(data){
 			console.log(data);
@@ -33,13 +33,12 @@ class App extends React.Component {
 
 	retrieveArticles() {
 		$.ajax({
-			url: 'http://ign-apis.herokuapp.com/articles/?startIndex=0&count=5',
+			url: 'http://ign-apis.herokuapp.com/articles/?startIndex=0&count=10',
 			type: 'GET',
 			dataType: 'json',
-			accept: 'application.json'
 		}).done(function(data) {
 			console.dir(data);
-			this.setState({data: data.data, activeClass: "articles", maxRows: 5, clickedRows: [], loaded: true});
+			this.setState({data: data.data, activeClass: "articles", maxRows: 10, clickedRows: [], loaded: true, hasLoadedMoreData: false});
 		}.bind(this))
 		.fail(function(data){
 			console.log(data);
@@ -47,14 +46,17 @@ class App extends React.Component {
 	}
 
 	loadMoreData() {
-		let maxRows = this.state.maxRows+5;
+		let maxRows = this.state.maxRows;
 		$.ajax({
-			url: 'http://ign-apis.herokuapp.com/'+(this.state.activeClass==='videos'?'videos':'articles')+'/?startIndex=0&count='+maxRows,
+			url: 'http://ign-apis.herokuapp.com/'+(this.state.activeClass==='videos'?'videos':'articles')+'/?startIndex='+this.state.maxRows+'&count='+maxRows,
 			type: 'GET',			
 			dataType: 'json'
 		}).done(function(data) {
-			console.dir(data);
-			this.setState({data: data.data, activeClass: (this.state.activeClass==='videos'?'videos':'articles'), maxRows: maxRows, index: maxRows-5});
+			let newData = this.state.data;
+			console.dir(this.state.data);
+			newData = newData.concat(data.data);
+			console.dir(newData);
+			this.setState({data: newData, activeClass: (this.state.activeClass==='videos'?'videos':'articles'), maxRows: maxRows, hasLoadedMoreData: true});
 		}.bind(this))
 		.fail(function(data){
 			console.log(data);
@@ -62,24 +64,12 @@ class App extends React.Component {
 	}
 
 	handleRowClick(row) {
-		let clickedRows = this.state.clickedRows;
-		if(clickedRows.indexOf(row) > -1) {
-			clickedRows.splice(clickedRows.indexOf(row), 1);
-		}
-		else {
-			clickedRows.push(row);
-		}
-		this.setState({clickedRow: clickedRows, index: row});
-		this.refs[this.state.index].scrollIntoView();
+		this.setState({clickedRow: row});
 	}
 
 	openImageUrl(url) {
 		var open = window.open(url, '_blank');
 		open.focus();
-	}
-
-	componentDidUpdate() {
-		this.refs[this.state.index].scrollIntoView();
 	}
 
 	componentDidMount() {
@@ -102,11 +92,11 @@ class App extends React.Component {
 		for(var i = 0; i < this.state.data.length && this.state.loaded; i++) {
 			var row = this.state.data[i];
 			rows.push(
-				<tr ref={i} key={i} onClick={this.handleRowClick.bind(this, i)}>
+				<tr style={{'display': this.state.clickedRow === i? 'none':''}} ref={i} key={i} onClick={this.handleRowClick.bind(this, i)}>
 					<td className="hover-bar"></td>
 					<td className="counter">{(count<10? "0":"")+count++}</td>
 					<td className="title">
-						{this.state.activeClass==='videos'? row.metadata.name:row.metadata.headline}
+						<div className="name">{this.state.activeClass==='videos'? row.metadata.name:row.metadata.headline}</div>
 						<div className="description">
 							{this.state.activeClass==='videos'? row.metadata.description:row.metadata.subHeadline}
 						</div>
@@ -115,8 +105,8 @@ class App extends React.Component {
 				</tr>
 			);
 			rows.push(	
-			 	<tr key={'image-'+i} style={{display: (this.state.clickedRows.indexOf(i) > -1? '':'none')}}>
-					<td style={{height: '100%', width: '100%', position: 'relative'}} colSpan="4">
+			 	<tr key={'image-'+i} style={{display: (this.state.clickedRow === i? '':'none')}}>
+					<td style={{height: '100%', width: '100%', position: 'relative', backgroundColor: 'black'}} colSpan="4">
 						<div style={{cursor: 'pointer'}} onClick={this.openImageUrl.bind(this, this.state.activeClass==='videos'? row.metadata.url:'http://www.ign.com/articles/'+row.metadata.slug)} className="goToIgn">GO TO IGN</div>
 						<div className="image-background">
 							<div className="image-container">
@@ -128,20 +118,22 @@ class App extends React.Component {
 			);	
 		}
 		return (
-			<div id="center">
-				<div id="content">
-					<div id="header">
-						<div onClick={this.retrieveVideos} id="videos" className={this.state.activeClass === "videos"? "active-header":"unactive-header"}>VIDEOS</div>
-						<div onClick={this.retrieveArticles} id="articles" className={this.state.activeClass === "articles"? "active-header":"unactive-header"}>ARTICLES</div>
+			<div style={{height: this.state.hasLoadedMoreData? 'auto':'100%'}} id="center">
+			<div id="content-background">
+					<div id="content">
+						<div id="header">
+							<div onClick={this.retrieveVideos} id="videos" className={this.state.activeClass === "videos"? "active-header":"unactive-header"}>VIDEOS</div>
+							<div onClick={this.retrieveArticles} id="articles" className={this.state.activeClass === "articles"? "active-header":"unactive-header"}>ARTICLES</div>
+						</div>
+						<div id="table-wrapper">
+							<table className="table">
+								<tbody>
+		    						{rows}
+		    					</tbody>
+					        </table>
+					    </div>
+				        {this.state.loaded && <div onClick={this.loadMoreData} id="load">{"SEE MORE " + (this.state.activeClass==="videos"? "VIDEOS...":"ARTICLES...")}</div>}    	
 					</div>
-					<div id="table-wrapper">
-						<table className="table">
-							<tbody>
-	    						{rows}
-	    					</tbody>
-				        </table>
-				    </div>
-			        {this.state.loaded && <div onClick={this.loadMoreData} id="load">{"SEE MORE " + (this.state.activeClass==="videos"? "VIDEOS...":"ARTICLES...")}</div>}    	
 				</div>
 			</div>
 		);
